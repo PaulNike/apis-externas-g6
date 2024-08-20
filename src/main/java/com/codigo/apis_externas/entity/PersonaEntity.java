d914dd1aa31509241e8ef544a04894d05019e200
NikeRodriguez <nike_1109@outlook.es>
2024-08-20 00:21:01 -0500

    fix: timestamp entity

--- src/main/java/com/codigo/apis_externas/entity/PersonaEntity.java
+++ src/main/java/com/codigo/apis_externas/entity/PersonaEntity.java
@@ -1,5 +1,6 @@
 package com.codigo.apis_externas.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,11 +22,11 @@ public class PersonaEntity {
     private String apellidos;
     private Integer estado;
     private String usua_crea;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Timestamp date_crea;
     private String usua_upda;
     private Timestamp date_upda;
     private String usua_dele;
     private Timestamp date_dele;
-
 
 }
