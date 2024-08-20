c9cf6518b3827da29eddf3b538cefe3e968c6ed3
NikeRodriguez <nike_1109@outlook.es>
2024-08-19 22:56:54 -0500

    Tema: Consumo de apis con OpenFeign

---
+++ src/main/java/com/codigo/apis_externas/aggregates/request/PersonaRequest.java
@@ -0,0 +1,13 @@
+package com.codigo.apis_externas.aggregates.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonaRequest {
+    private String numdoc;
+    private String nombres;
+    private String apellidos;
+    //SOLID
+}
